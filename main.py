6ca3fa55fc5915085eddd279a45e242e4ed09895
Vinicius Augusto <vinicius_augusto_@msn.com>
2022-06-21 12:59:07 -0300

    [style] Implementing PEP 8 standards for code and some pythonic adjustments.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import pandas as pd
 
-
 xlsx_file_names = []
+
 
 def print_sheet_comparison(sheet1, sheet2, sheet_name):
     print('---')
     print(sheet1 == sheet2)
     print('---------- X ----------\n')
+
 
 for file in os.listdir('./'):
     if file.endswith('.xlsx'):
@@ -20,11 +21,5 @@ if __name__ == '__main__':
         df1 = pd.read_excel(xlsx_file_names[0], sheet_names[i])
         df2 = pd.read_excel(xlsx_file_names[1], sheet_names[i])
         print('Result of \'' + sheet_names[i] + '\': ' + str(df1.equals(df2)))
-        if (df1.equals(df2) == False):
+        if not df1.equals(df2):
             print_sheet_comparison(df1, df2, sheet_names[i])
-
-
-
-
-
-
