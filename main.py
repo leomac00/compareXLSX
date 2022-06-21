ced52b773fe5c5b8591e81b9c271d819e79da5b8
lomh <leonardo.machado@gft.com>
2022-06-21 13:35:14 -0300

    Removing unecessary parameter in print_sheet_comparison

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 xlsx_file_names = []
 
 
-def print_sheet_comparison(sheet1, sheet2, sheet_name):
+def print_sheet_comparison(sheet1, sheet2):
     print('---')
     print(sheet1 == sheet2)
     print('---------- X ----------\n')
@@ -22,4 +22,4 @@ if __name__ == '__main__':
         df2 = pd.read_excel(xlsx_file_names[1], sheet_names[i])
         print('Result of \'' + sheet_names[i] + '\': ' + str(df1.equals(df2)))
         if not df1.equals(df2):
-            print_sheet_comparison(df1, df2, sheet_names[i])
+            print_sheet_comparison(df1, df2)
