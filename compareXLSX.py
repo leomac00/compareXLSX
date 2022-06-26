23c4892d9804ebd68651838dc87c19d106b9fa02
lomh <leonardo.machado@gft.com>
2022-06-26 18:41:25 -0300

    Method extraction from body of MAIN

--- compareXLSX.py
+++ compareXLSX.py
@@ -3,7 +3,9 @@ import numpy as np
 import copy
 import os
 
-
+def print_sheets_comparison(names_list1, names_list2, names_intersection):
+    differences = (set(names_list1).difference(set(names_intersection))).union((set(names_list2).difference(set(names_intersection))))
+    print(' - Mismatching sheets found: ', differences)
 
 def print_values_comparison(sheet1, sheet2):
     print('---------- X ----------')
@@ -27,19 +29,19 @@ if __name__ == '__main__':
     #Comparing sheets inside workbook and getting the intersected items to be compared next
     sheet_names1 = pd.ExcelFile(xlsx_file_names[0]).sheet_names
     sheet_names2 = pd.ExcelFile(xlsx_file_names[1]).sheet_names
-    sheet_names = list(set(sheet_names1) & set(sheet_names2))
+    sheet_names_intersection = list(set(sheet_names1) & set(sheet_names2))
     are_sheets_equal = sheet_names1 == sheet_names2
     print('\n--->  Comparing sheets inside both workbooks...')
     print('Same sheets between both items? -> ' + str(are_sheets_equal))
     if not are_sheets_equal:
-        differences = (set(sheet_names1).difference(set(sheet_names))).union((set(sheet_names2).difference(set(sheet_names))))
-        print(' - Mismatching sheets found: ', differences)
-        
+        print_sheets_comparison(sheet_names1, sheet_names2, sheet_names_intersection)
+
     #Comparing values inside each sheet
     print('\n--->   Comparing values inside each sheet...')
-    for i in range(len(sheet_names)):
-        df1 = pd.read_excel(xlsx_file_names[0], sheet_names[i])
-        df2 = pd.read_excel(xlsx_file_names[1], sheet_names[i])
-        print('Both sheets \'' + sheet_names[i] + '\' match? -> ' + str(df1.equals(df2)))
+    for i in range(len(sheet_names_intersection)):
+        df1 = pd.read_excel(xlsx_file_names[0], sheet_names_intersection[i])
+        df2 = pd.read_excel(xlsx_file_names[1], sheet_names_intersection[i])
+        are_values_equal = df1.equals(df2)
+        print('Both sheets \'' + sheet_names_intersection[i] + '\' match? -> ' + str(are_values_equal))
         if not df1.equals(df2):
             print_values_comparison(df1, df2)
