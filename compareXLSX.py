1b0c8537b496689a6b1d59d7ffac38fd678d8add
lomh <leonardo.machado@gft.com>
2022-07-20 16:04:03 -0300

    Dealing with NaN

--- compareXLSX.py
+++ compareXLSX.py
@@ -14,14 +14,16 @@ def print_values_comparison(sheet1, sheet2):
     print('---------- X ----------')
     try:
         comparison_values = sheet1.values == sheet2.values
-        rows, cols = np.where(comparison_values is False and comparison_values is not np.nan)
+        rows, cols = np.where(comparison_values == False)
         copied_sheet = copy.deepcopy(sheet1)
         for item in zip(rows, cols):
+            if item is np.nan:
+                continue
             copied_sheet.iloc[item[0], item[1]] = '[{}]~[{}]'.format(sheet1.iloc[item[0], item[1]],
                                                                      sheet2.iloc[item[0], item[1]])
         print(copied_sheet)
-    except Exception as e :
-        print("The files could not be compared, this happened:\n-->" + e)
+    except Exception as e:
+        print("The files could not be compared, this happened:\n-->" + str(e))
     finally:
         print('---------- X ----------')
 
